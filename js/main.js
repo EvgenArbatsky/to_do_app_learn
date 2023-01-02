fccfbc8782f5b5d26ff5ab9e411cbf54679f4ecb
Arbatsky <moscowgoodhit@gmail.com>
2023-01-02 15:27:09 +0300

    Реализовано сохранение и загрузка из localStorage

--- js/main.js
+++ js/main.js
@@ -5,7 +5,33 @@ const tasksList = document.querySelector('#tasksList');
 const emptyList = document.querySelector('#emptyList');
 
 let tasks = [];
-checkEmptyList()
+
+if (localStorage.getItem('tasks')) {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+tasks.forEach(function (task) {
+    // Формируем CSS класс
+    const cssClass = task.done ? 'task-title task-title--done' : 'task-title';
+    // Формируем разметку для новой задачи
+    const taskHTML = `
+				<li id="${task.id}" class="list-group-item d-flex justify-content-between task-item">
+					<span class="${cssClass}">${task.text}</span>
+					<div class="task-item__buttons">
+						<button type="button" data-action="done" class="btn-action">
+							<img src="./img/tick.svg" alt="Done" width="18" height="18">
+						</button>
+						<button type="button" data-action="delete" class="btn-action">
+							<img src="./img/cross.svg" alt="Done" width="18" height="18">
+						</button>
+					</div>
+				</li>
+    `
+    // Добавляем задачу на страницу
+    tasksList.insertAdjacentHTML('beforeend', taskHTML);
+})
+
+checkEmptyList();
 
 form.addEventListener('submit', addTask);
 tasksList.addEventListener('click', deleteTask)
@@ -24,7 +50,8 @@ function addTask(event) {
         done: false
     }
     // Добавляем объект в массив с задачами
-    tasks.push(newTask)
+    tasks.push(newTask);
+    saveToLocalStorage();
     // Формируем CSS класс
     const cssClass = newTask.done ? 'task-title task-title--done' : 'task-title';
 
@@ -65,6 +92,7 @@ function deleteTask(event) {
         })
         // Удаляем задачу из массива с задачами
         tasks.splice(index, 1);
+        saveLocalStorage();
         // Удаляем задачу из разметки
         parentNode.remove();
 
@@ -83,7 +111,9 @@ function doneTask(event) {
                 return true
             }
         })
-        task.done = !task.done
+        task.done = !task.done;
+
+        saveToLocalStorage();
 
         const taskTitle = parentNode.querySelector('.task-title');
         taskTitle.classList.toggle('task-title--done')
@@ -105,4 +135,8 @@ function checkEmptyList() {
         const emptyListEl = document.querySelector('#emptyList');
         emptyListEl ? emptyListEl.remove() : null;
     }
+}
+
+function saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(tasks))
 }
